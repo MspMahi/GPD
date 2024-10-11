b7c522fcfc6792a3083d6c7a9c46e66504216fe8
Mahi <Mahi@192.168.1.17>
2024-10-11 09:10:36 +0530

    Class name was changed

--- MSproj1-Eureka/src/main/java/com/nt/MsEurekaServer.java
+++ MSproj1-Eureka/src/main/java/com/nt/MsEurekaServer.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class MSproj1EurekaApplication {
+public class MsEurekaServer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MSproj1EurekaApplication.class, args);
+		SpringApplication.run(MsEurekaServer.class, args);
 	}
 
 }
